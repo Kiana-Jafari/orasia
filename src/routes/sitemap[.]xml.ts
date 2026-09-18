26c1a61066a77cc775f4ebff84072831bd268d06
Kiana <kiana.jafari.ai@gmail.com>
2026-09-18 18:54:31 +0330

    Site index

---
+++ src/routes/sitemap[.]xml.ts
@@ -0,0 +1,40 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/sitemap.xml")({
+    server: {
+        handlers: {
+            GET: () => {
+                const baseUrl = "https://orasiatrade.com";
+
+                const urls = [
+                    "/",
+                    "/about",
+                    "/contact",
+                    "/products/import-product-01",
+                    "/products/import-product-02",
+                    "/products/import-product-03",
+                    "/products/export-product-01",
+                    "/products/export-product-02",
+                    "/products/export-product-03",
+                ];
+
+                const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
+                        .map(
+                            (path) => `  <url>
+    <loc>${baseUrl}${path}</loc>
+  </url>`,
+                        )
+                        .join("\n")}
+</urlset>`;
+
+                return new Response(sitemap, {
+                    headers: {
+                        "Content-Type": "application/xml",
+                    },
+                });
+            },
+        },
+    },
+});
